c5abbe3ba1696de09cddf20cd9dbee6be26d3f87
devfel <devfelizardo@gmail.com>
2024-04-06 14:28:17 -0300

    changing LoadingIndicator to Image Component

--- src/app/components/LoadingIndicator.js
+++ src/app/components/LoadingIndicator.js
@@ -1,9 +1,11 @@
 // project/src/components/LoadingIndicator.js
 import React from "react";
+import Image from "next/image";
 import loadingGif from "../../../public/loading.gif";
+
 const LoadingIndicator = () => (
-  <div>
-    <img src={loadingGif} alt="Loading..." />
+  <div style={{ width: 25, height: 25, position: "relative" }}>
+    <Image src={loadingGif} alt="Loading..." layout="fill" objectFit="contain" />
   </div>
 );
 

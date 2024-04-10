35799d2c57e239df6064758877d174012916796e
devfel <devfelizardo@gmail.com>
2024-04-09 23:31:32 -0300

    importing loading indicator component

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../../context/AuthContext";
 import Link from "next/link";
+import LoadingIndicator from "../../components/LoadingIndicator";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");

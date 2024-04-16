16606e528fa8236535fe333d50dddc595d77dcef
devfel <devfelizardo@gmail.com>
2024-04-16 04:15:54 -0300

    changing css for search and sort, removing focus ring

--- src/app/page.js
+++ src/app/page.js
@@ -129,13 +129,13 @@ export default function Home() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
               </svg>
             </span>
-            <input className="text-black pl-10 pr-4 py-2 w-full md:w-auto max-w-80 rounded-lg shadow focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" placeholder="Buscar jogo..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+            <input className="text-black pl-10 pr-4 py-2 border border-gray-300 w-full md:w-auto max-w-80 rounded-lg shadow focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" placeholder="Buscar jogo..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
           </div>
 
           <div className="flex items-center space-x-2">
-            <label className="text-sm font-medium text-gray-700">Ordenar:</label>
+            <label className="text-sm font-medium text-gray-600">Ordenar:</label>
             <div className="relative">
-              <select className="appearance-none bg-white border border-gray-300 text-gray-700 py-2 px-4 pr-8 rounded-lg shadow focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full md:w-56" value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
+              <select className="appearance-none bg-white border border-gray-300 text-gray-600 py-2 px-4 pr-8 rounded-lg shadow focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full md:w-56" value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
                 <option value="mostRecent">Mais Recente Primeiro</option>
                 <option value="oldFirst">Mais Antigo Primeiro</option>
                 <option value="priceDesc">Preço (Maior Primeiro)</option>
